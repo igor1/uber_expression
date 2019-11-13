03826a4ecee51d8bf4640850323d0f060df821c0
Igor Slepchin <igor.slepchin@gmail.com>
2019-11-12 19:33:40 -0500

    working

--- comp.py
+++ comp.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 def is_int(e):
     try:
         int(e)
@@ -24,6 +26,7 @@ def stack_compute(stack, op2):
     val = compute(op1, op, op2)
     stack = stack[:-2]
     stack.append(val)
+    return stack
 
 def calculate(expr):
     stack = []
@@ -34,15 +37,7 @@ def calculate(expr):
             if not stack or stack[-1] == '(':
                 stack.append(int(e))
             else:
-                stack_compute(stack, int(e))
-#                assert is_op(stack[-1]), f"Bad input - not an op: {stack[-1]}, stack: {stack}, e: {e}"
-#                assert is_int(stack[-2]), f"Bad input - not an int: {stack[-2]}"
-#                op1 = stack[-2]
-#                op2 = int(e)
-#                op = stack[-1]
-#                val = compute(op1, op, op2)
-#                stack = stack[:-2]
-#                stack.append(val)
+                stack = stack_compute(stack, int(e))
         elif is_op(e):
             stack.append(e)
         elif e == "(":
@@ -53,7 +48,7 @@ def calculate(expr):
             assert stack[-2] == "(", f"Bad input - expected (, got {stack[-2]}"
             stack = stack[:-2]
             if stack:
-                stack_compute(stack, val)
+                stack = stack_compute(stack, val)
             else:
                 stack.append(val)
         else:
@@ -64,7 +59,6 @@ def calculate(expr):
             
             
 if __name__ == "__main__":
-    result = calculate("1 + (2 + 3)")
-    print(f"result: {result}")
-    
-    
+    expr = "1 - (2 - (3+4))"
+    result = calculate(expr)
+    print(f"{expr}={result}")
